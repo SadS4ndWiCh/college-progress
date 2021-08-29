594a5bb7a128daff8d2346ede5ad484b861699be
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-08-29 19:12:13 -0300

    add: meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import type { NextPage } from 'next'
+import Head from 'next/head';
+
+import ogImage from '@public/images/og-image.webp';
 
 import { Countdown } from '@components/Countdown';
 
@@ -7,6 +10,28 @@ import styles from '@styles/pages/Home.module.scss';
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      <Head>
+        <meta name="title" content="ADS Fatec - Contagem Regressiva" />
+        <meta name="description" content="Contagem regressiva para o termino da faculdade de ADS" />
+        <meta property="og:image" content={`https://adsfatec.vercel.app/images/${ogImage}`} />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://adsfatec.vercel.app/" />
+        <meta property="og:title" content="ADS Fatec - Contagem Regressiva" />
+        <meta property="og:description" content="Contagem regressiva para o termino da faculdade de ADS" />
+        <meta property="og:image" content="https://adsfatec.vercel.app/assets/og-image.webp" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://adsfatec.vercel.app/" />
+        <meta property="twitter:title" content="ADS Fatec - Contagem Regressiva" />
+        <meta property="twitter:description" content="Contagem regressiva para o termino da faculdade de ADS" />
+        <meta property="twitter:image" content="https://adsfatec.vercel.app/assets/og-image.webp" />
+
+        <title>ADS Fatec - Contagem Regressiva</title>
+      </Head>
+
       <header>
         <div>
           <h1><span>ADS</span> Fatec</h1>
